ced6e0af32bd92e05e6b810303e2fc33e67d22b6
Wyndev <108705186+WyndevCodes@users.noreply.github.com>
2025-08-05 21:06:09 -0500

    Feat: mana bar works on Hypixel Skyblock, more advanced server connection logic internally

--- src/main/java/io/github/pikibanana/dungeonapi/DungeonDodgeConnection.java
+++ src/main/java/io/github/pikibanana/dungeonapi/DungeonDodgeConnection.java
@@ -12,32 +12,58 @@ import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
 
 public class DungeonDodgeConnection {
 
     private static boolean isConnected = false;
+    private static String connectedServerAddress;
     private boolean isToggled = false;
     private int hiddenTogglePet = 0;
 
+    /**
+     * @return True if the client is connected to DungeonDodge, false otherwise
+     */
     public static boolean isConnected() {
         return isConnected;
     }
 
+    /**
+     * Checks if the client is logged in to a specific server.<br>
+     * In reality, this checks if the server address of the
+     * remote server the client has connected to contains
+     * the specified string, ignoring case.
+     * @param serverAddressContains The portion of the client's
+     * connected server address to check for, e.g. "dungeondodge"
+     * @return True if the client is connected to the server with
+     * the inputted address portion, false otherwise
+     */
+    public static boolean isOnServer(@NotNull String serverAddressContains) {
+        return connectedServerAddress != null && connectedServerAddress.toLowerCase().contains(serverAddressContains.toLowerCase());
+    }
+
     public void onJoin(ClientPlayNetworkHandler handler, PacketSender sender, MinecraftClient client) {
-        if (handler.getServerInfo() != null && handler.getServerInfo().address.contains("dungeondodge")) {
-            isConnected = true;
+        if (handler.getServerInfo() != null) {
 
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            if (player != null && DungeonDodgePlusConfig.get().features.autoTogglePet.enabled && !isToggled) {
-                player.networkHandler.sendCommand("togglepet");
-                isToggled = true;
+            connectedServerAddress = handler.getServerInfo().address;
 
-                UpdateChecker updateChecker = new UpdateChecker();
-                boolean isUpdateAvailable = updateChecker.isNewVersionAvailable(UpdateChecker.latestVersionNumber);
+            //DungeonDodge specific logic
+            if (isOnServer("dungeondodge")) {
+                isConnected = true;
 
-                if (isUpdateAvailable) {
-                    TaskScheduler.scheduleDelayedTask(100, this::sendUpdateMessageToChat);
+                ClientPlayerEntity player = MinecraftClient.getInstance().player;
+                if (player != null && DungeonDodgePlusConfig.get().features.autoTogglePet.enabled && !isToggled) {
+                    player.networkHandler.sendCommand("togglepet");
+                    isToggled = true;
                 }
+            }
+
+            //auto-update
+            UpdateChecker updateChecker = new UpdateChecker();
+            boolean isUpdateAvailable = updateChecker.isNewVersionAvailable(UpdateChecker.latestVersionNumber);
+
+            if (isUpdateAvailable) {
+                TaskScheduler.scheduleDelayedTask(100, this::sendUpdateMessageToChat);
             }
         }
     }
